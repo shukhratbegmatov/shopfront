899b50f62443dfaab5613054cfd47c24e456d218
Shuxrat <shuukhratbegmatov@gmail.com>
2024-11-05 17:15:39 +0500

    fix

---
+++ precache-manifest.8d2042ddfe6da95c7d5029721b42cda0.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b4487cc79b7f0b2bbeccbe4975ef65f",
+    "url": "/222.obj"
+  },
+  {
+    "revision": "b70ff48dda870827b84e335108f10944",
+    "url": "/bgright.png"
+  },
+  {
+    "revision": "75bfafe78bc2a168f5a3",
+    "url": "/css/app.dbd050a7.css"
+  },
+  {
+    "revision": "04bb469ad110e6812de9",
+    "url": "/css/chunk-vendors.7cd5a820.css"
+  },
+  {
+    "revision": "b952c3c81ba34b54c66c748ea1e828a7",
+    "url": "/fonts/Raleway-Medium.b952c3c8.ttf"
+  },
+  {
+    "revision": "b70ff48dda870827b84e335108f10944",
+    "url": "/img/bgright.b70ff48d.png"
+  },
+  {
+    "revision": "0c575cec7a5d93ba3e0a4c023ab6da1b",
+    "url": "/img/catebg.0c575cec.png"
+  },
+  {
+    "revision": "d4d636c3f08e37063240e9475ac25b91",
+    "url": "/img/image.d4d636c3.png"
+  },
+  {
+    "revision": "68a29180c3303b71a07702daf1f6b2c8",
+    "url": "/img/kisspng.68a29180.png"
+  },
+  {
+    "revision": "721e13ff548caab70cf543318446dca0",
+    "url": "/img/koza.obj"
+  },
+  {
+    "revision": "8ad23a631835a95871c9aa8633d10ebc",
+    "url": "/img/loginbg.8ad23a63.png"
+  },
+  {
+    "revision": "cd56628a96c35ebfc79b17c1d8281ef9",
+    "url": "/img/loginbottom.cd56628a.png"
+  },
+  {
+    "revision": "c86d31c4a3b40dbdc3c1f642b57f3ff7",
+    "url": "/img/loginrightbg.c86d31c4.png"
+  },
+  {
+    "revision": "3987208fc7714b0e0d35a70ffdd47160",
+    "url": "/img/secbg.3987208f.png"
+  },
+  {
+    "revision": "27343074095e6e8464193f9f9bff3719",
+    "url": "/index.html"
+  },
+  {
+    "revision": "769dd1d09429d8f767b5",
+    "url": "/js/about.4b3c6576.js"
+  },
+  {
+    "revision": "75bfafe78bc2a168f5a3",
+    "url": "/js/app.44ba4bd9.js"
+  },
+  {
+    "revision": "04bb469ad110e6812de9",
+    "url": "/js/chunk-vendors.4e1498dc.js"
+  },
+  {
+    "revision": "721e13ff548caab70cf543318446dca0",
+    "url": "/koza.obj"
+  },
+  {
+    "revision": "8f92aca49797b6fb22e11b653e2c3301",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "34bea533829944fc920af60a9e9001f2",
+    "url": "/model_2fbx.fbx"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
